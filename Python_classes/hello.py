3b4a2c2712cd6683020e8c8bfef8ed8fb802e9fd
Cyb3rch37 <gitlab.frame027@passinbox.com>
2025-07-16 21:48:15 +0300

    initial commitfor my python classes

---
+++ Python_classes/hello.py
@@ -0,0 +1,18 @@
+print("Hello, World!") #This is a comment
+
+
+#sample code to print python version
+import sys
+print(sys.version)
+
+#identation in python indicate a block of code
+if 5 > 2:
+  print("Five is greater than two!") 
+
+#the hash sign is used to create a comment in python
+
+#Multi line commenting
+"""Since Python will ignore string literals 
+that are not assigned to a variable, 
+you can add a multiline string (triple quotes) 
+in your code, and place your comment inside it:"""
